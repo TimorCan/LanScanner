7924d2c0a389a5899cae4ed0287f2994ee6394f2
Dung Vu <hoangdung00275@gmail.com>
2023-08-11 20:47:01 +0700

    [Framework] First Commit

---
+++ Sources/LanScanner/include/LanScanner.h
@@ -0,0 +1,20 @@
+//
+//  LanScanner.h
+//  LanScanner
+//
+//  Created by Macbook 2018 on 11/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LanScanner.
+FOUNDATION_EXPORT double LanScannerVersionNumber;
+
+//! Project version string for LanScanner.
+FOUNDATION_EXPORT const unsigned char LanScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LanScanner/PublicHeader.h>
+#import <LanScanner/MMDevice.h>
+#import <LanScanner/MMLANScanner.h>
+
+
