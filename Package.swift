6bafbe468f5468fa3b902fecf5aeb83a3eeaef37
Dung Vu <hoangdung00275@gmail.com>
2023-08-11 22:02:12 +0700

    Update 4

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             type: .static,
             targets: ["LanScanner"]),
     ],
-    dependencies: [],
+    dependencies:[],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -25,5 +25,5 @@ let package = Package(
         .testTarget(
             name: "LanScannerTests",
             dependencies: ["LanScanner"]),
-    ]
+    ], cLanguageStandard: .gnu11
 )
