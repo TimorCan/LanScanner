e9d2fa66a0d1fab0eb7be97375dc90f2eeee9a4d
Dung Vu <hoangdung00275@gmail.com>
2023-08-12 13:03:48 +0700

    Update 12

--- Sources/SwiftLanScanner/SwiftLanScanner.swift
+++ Sources/SwiftLanScanner/SwiftLanScanner.swift
@@ -21,6 +21,26 @@ public class SwiftLanScanner: NSObject {
     @Published private var _listDevice: [MMDevice] = []
     @Published private var _progress: Float = 0
     
+    // MARK: -- API Information
+    private struct API {
+        static let key = "01h7m2b59pfxhmgbts56kv013y01h7m2btsc3dv6kqz0sw0qqkdpmhsfvk7sjpwy"
+        static let baseURL = "https://api.maclookup.app/v2/macs/"
+        
+        struct Parameters {
+            var macAddress: String
+            func makeRequest() throws -> URLRequest {
+                var components = URLComponents(string: API.baseURL + macAddress)
+                components?.queryItems = [.init(name: "apiKey", value: API.key)]
+                guard let url = components?.url else {
+                    throw NSError(domain: NSURLErrorDomain, code: -1)
+                }
+                let req = URLRequest(url: url)
+                return req
+            }
+        }
+    }
+    
+    // MARK: -- Proceduce Log Information
     private struct Log {
         static func output(at f:String = #function, line: Int = #line, detail: Any...) {
             print("""
@@ -32,6 +52,7 @@ public class SwiftLanScanner: NSObject {
         }
     }
     
+    /// Hide init avoid custom
     private override init() {
         super.init()
     }
@@ -54,27 +75,55 @@ public class SwiftLanScanner: NSObject {
         self._scanner?.stop()
     }
     
-    
     // MARK: -- Public Function
+    /// State scan
     public static var isScanning: Bool {
         return shared._isScanning
     }
     
+    /// Get progess scan
     public static var progress: AnyPublisher<Float, Never> {
         return shared.$_progress.eraseToAnyPublisher()
     }
     
+    /// Get list device received
     public static var listDevice: AnyPublisher<[MMDevice], Never> {
         return shared.$_listDevice.eraseToAnyPublisher()
     }
     
+    /// Start scan
     public static func start() {
         // Clear device
         shared._start()
     }
     
+    
+    /// Stop scan
     public static func stop() {
         shared._stop()
+    }
+    
+    // MARK: - LookupAPIResponse
+   public struct LookupAPIResponse: Codable {
+        public let success, found: Bool?
+        public let macPrefix, company, address, country: String?
+        public let blockStart, blockEnd: String?
+        public let blockSize: Int?
+        public let blockType, updated: String?
+        public let isRand, isPrivate: Bool?
+        public let error: String?
+        public let errorCode: Int?
+        public let moreInfo: String?
+    }
+    
+    // MARK: -- Request Information from API
+    /// Request Information manufacture from macaddress of device
+    /// - Parameter macaddress: String
+    /// - Returns: Model has information include : url ,  relative ..
+    public static func requestInformation(macaddress: String) async throws -> LookupAPIResponse {
+        let p = API.Parameters(macAddress: macaddress)
+        let response = try await URLSession.shared.data(for: try p.makeRequest())
+        return try JSONDecoder().decode(LookupAPIResponse.self, from: response.0)
     }
 }
 
